edc7b81e37bb8d2632be403dd0498f165f2988d2
Your Name <you@example.com>
2017-03-31 15:49:27 +1300

    Removed SciPy dependence

--- hourly_wage.py
+++ hourly_wage.py
@@ -1,141 +1,152 @@
-r"""
-The functions below compute gross earnings from net earnings subject to
-New Zealand income tax rates for April 2014 to March 2015.
+import textwrap
 
-AUTHORS:
-
-- Alex Raichev (2013-03-19)
-"""
-from scipy.optimize import brentq
-
-# April 2014 to March 2015 New Zealand income tax tiers for individuals 
-# and their corresponding tax rates.
-NZ_TAX_TIERS_2014 = [
+# New Zealand income tax tiers (cutoff value, tax rate) for 2016-04-01 to 2017-03-31
+BIG = 10e12
+NZ_TAX_TIERS = [
   (14000, 0.105), 
   (48000, 0.175), 
   (70000, 0.3), 
-  (10e12, 0.33)
+  (BIG, 0.33)
 ]
-WORK_WEEKS_PER_YEAR = 47 # Considering 4 weeks annual leave and holidays
+# Work weeks per year counting 4 weeks paid leave and 2 weeks paid holiday
+WORK_WEEKS_PER_YEAR = 46 
 
-def tax(gross_yearly_income, tax_tiers=NZ_TAX_TIERS_2014, ndigits=2):
-    r"""
-    Return the income tax due and the effective tax rate on the given 
-    gross yearly income.    
-    Use the given tax tiers and round results to `ndigits` decimal places.
-    Assume `gross_yearly_income >= 1`.
 
-    EXAMPLES::
-
-        >>> print(tax(13999, ndigits=3))
-        (1469.895, 0.105)
-        >>> print(tax(14000, ndigits=3))
-        (1470.0, 0.105)
-        >>> print(tax(15000, ndigits=3))
-        (1645.0, 0.11)
-        >>> print(tax(65238))
-        (12591.4, 0.19)
-        >>> print(tax(45000))
-        (6895.0, 0.15)
+def mindex(x, cutoffs):
     """
-    gyi = gross_yearly_income
-    # Ensure good input.
-    if gyi < 1:
-        gyi = 1
-    result = 0.0
-    previous_cutoff = 0
-    for (cutoff, tax_rate) in tax_tiers:
-        if gyi < cutoff:
-            result += (gyi - previous_cutoff)*tax_rate
-            break
-        else:
-            result += (cutoff - previous_cutoff)*tax_rate
-        previous_cutoff = cutoff
-    return round(result, ndigits), round(result/gyi, ndigits)
+    Given a number ``x`` and an increasing list of numbers ``cutoffs``, return the least index i such that such that x < cutoffs[i].
+    
+    EXAMPLES::
 
-def tax_h(gross_hourly_income, hours_per_week, 
-  work_weeks_per_year=WORK_WEEKS_PER_YEAR, tax_tiers=NZ_TAX_TIERS_2014,
-  ndigits=2):
-    r"""
-    Return the income tax due and the effective tax rate on the 
-    gross yearly income `gross_hourly_income*hours_per_week*weeks_per_year`.
-    Use the given tax tiers and round all results to `ndigits` decimal  
-    places.
+        >>> cutoffs = [14000, 48000, 70000, 10e12]
+        >>> mindex(0, cutoffs)
+        0
+        >>> mindex(14000, cutoffs)
+        1
+        >>> mindex(70001, cutoffs)
+        3
+    """
+    for i, cutoff in enumerate(cutoffs):
+        if x < cutoff:
+            break
+    return i 
+
+def tax_and_rate(gross_yearly_income, tax_tiers=NZ_TAX_TIERS):
+    """
+    Return the income tax due and the effective tax rate on the given 
+    gross yearly income using the given tax tiers.
 
     EXAMPLES::
 
-        >>> print(tax_h(75, 20))
-        (14185.0, 0.2)
+        >>> tax_and_rate(-7)
+        (0, 0)
+        >>> tax_and_rate(13999)
+        (1469.895, 0.105)
+        >>> tax_and_rate(14000)
+        (1470.0, 0.105)
+        >>> tax_and_rate(15000)
+        (1645.0, 0.10966666666666666)
+        >>> tax_and_rate(65238)
+        (12591.399999999998, 0.19300714307612124)
+        >>> tax_and_rate(45000)
+        (6894.999999999999, 0.1532222222222222)
+    """
+    x = gross_yearly_income
+
+    if x <= 0:
+        return 0, 0
+
+    cutoffs, rates = zip(*tax_tiers)
+
+    # Apply tax formula
+    k = mindex(x, cutoffs)
+    t = sum([(rates[i] - rates[i + 1])*cutoffs[i] for i in range(k)]) +\
+      rates[k]*x
+
+    return t, t/x
+
+def tax_and_rate_h(gross_hourly_income, hours_per_week, 
+  work_weeks_per_year=WORK_WEEKS_PER_YEAR, tax_tiers=NZ_TAX_TIERS):
+    """
+    Return the income tax due and the effective tax rate on the gross yearly income ``gross_hourly_income*hours_per_week*weeks_per_year`` using the given tax tiers.
+
+    EXAMPLES::
+
+        >>> tax_and_rate_h(75, 20)
+        (13720.0, 0.19884057971014493)
     """
     gyi = gross_hourly_income*hours_per_week*work_weeks_per_year
-    return tax(gyi, tax_tiers=tax_tiers, ndigits=ndigits)
+    return tax_and_rate(gyi, tax_tiers=tax_tiers)
 
-def gross_yearly_income(net_yearly_income, tax_tiers=NZ_TAX_TIERS_2014, 
-  ndigits=2):
-    r"""
+def gross_yearly_income(net_yearly_income, tax_tiers=NZ_TAX_TIERS):
+    """
     Return the gross yearly income required to earn the given   
     net yearly income after subtracting income tax specified by the given 
     tax tiers.
-    Round result to `ndigits` decimal places.
     
     EXAMPLES::
     
         >>> nyi = 50000
         >>> gyi = gross_yearly_income(nyi)
-        >>> print(gyi)
-        61457.14
-        >>> nyi_get = gyi - tax(gyi)[0]
-        >>> print(nyi, nyi_get)
-        50000 50000.0
+        >>> gyi
+        61457.14285714286
+        >>> nyi_get = gyi - tax_and_rate(gyi)[0]
+        >>> nyi == nyi_get
+        True
     """                    
-    def f(y):
-        return y - tax(y, tax_tiers=tax_tiers)[0] - net_yearly_income
-    # The gross yearly income is the zero of f.
-    gyi = brentq(f, 1, 1e9)  
-    return round(gyi, ndigits)
+    y = net_yearly_income
+
+    cutoffs, rates = zip(*tax_tiers)
+    inverse_cutoffs = [c - tax_and_rate(c)[0] for c in cutoffs]
+    k = mindex(y, inverse_cutoffs)
+    x = (y + sum([(rates[i] - rates[i + 1])*cutoffs[i] for i in range(k)]))/\
+      (1 - rates[k])
+    return x
 
 def gross_hourly_income(net_weekly_income, hours_per_week, 
-  work_weeks_per_year=WORK_WEEKS_PER_YEAR, tax_tiers=NZ_TAX_TIERS_2014,
-  ndigits=2):
-    r"""
+  work_weeks_per_year=WORK_WEEKS_PER_YEAR, tax_tiers=NZ_TAX_TIERS):
+    """
     Return the gross hourly income required to earn the given net weekly 
-    income (for 52 weeks per year) when working the given number of hours 
+    income for *52* weeks per year when working the given number of hours 
     per week for the given number of weeks per year after subtracting income
     tax specified by the given tax tiers.
-    Round result to `ndigits` decimal places.
 
     EXAMPLES::
 
         >>> net_weekly_income = 1200
-        >>> hours_per_week = 20
-        >>> weeks_per_year = 47
-        >>> ghi = gross_hourly_income(net_weekly_income, hours_per_week, weeks_per_year) 
-        >>> print(ghi)
-        84.66
-        >>> gyi = ghi*hours_per_week*weeks_per_year
-        >>> tax, tax_rate = tax(gyi)
+        >>> hours = 20
+        >>> weeks = 47
+        >>> ghi = gross_hourly_income(net_weekly_income, hours, weeks) 
+        >>> ghi
+        84.66179739599875
+        >>> gyi = ghi*hours*weeks
+        >>> tax, rate = tax_and_rate(gyi)
         >>> nyi = gyi - tax
-        >>> net_weekly_income_get = round(nyi/52, 2)
-        >>> print(net_weekly_income, net_weekly_income_get)
-        1200 1199.98
-        >>> print(tax_rate, tax_h(ghi, hours_per_week)[1])
-        0.22 0.22
+        >>> net_weekly_income_get = round(nyi/52)
+        >>> net_weekly_income == net_weekly_income_get
+        True
+        >>> rate == tax_and_rate_h(ghi, hours, weeks)[1]
+        True
     """    
     gyi = gross_yearly_income(net_weekly_income*52)
     ghi = gyi/(hours_per_week*work_weeks_per_year)
-    return round(ghi, ndigits)
-
-HOURS = [5*i for i in range(1, 9)]
-NET_WEEKLY_INCOMES = [100*i for i in range(1, 26)]
+    return ghi
 
 def max_len(items):
     """
     Return the length of the longest item (when converted to a string) 
     in the given list.
+
+    EXAMPLES::
+
+        >>> max_len(['', 'oh', 'boy', 'oh'])
+        3
     """
     return max(len(str(item)) for item in items)
 
-def print_table(hours=HOURS, net_weekly_incomes=NET_WEEKLY_INCOMES):
+def print_table(hours=tuple(5*i for i in range(1, 9)), 
+  net_weekly_incomes=tuple(100*i for i in range(1, 26)), 
+  weeks=WORK_WEEKS_PER_YEAR, tax_tiers=NZ_TAX_TIERS):
     # import pandas as pd
     #
     # nwis = [(x, 52*x) for x in net_weekly_incomes]
@@ -150,15 +161,14 @@ def print_table(hours=HOURS, net_weekly_incomes=NET_WEEKLY_INCOMES):
     #   -------------------------------------------------------"""))
     # print(df)
 
-    # Print without Pandas to reduce third-party module dependecies. 
-    import textwrap
-    
+    # Print without Pandas to reduce third-party module dependecies.     
     # Create the table
     nwis = net_weekly_incomes
     col_header = [' '] + [str(h) for h in hours]
-    table = [['($%s, $%s)' % (nwi, 52*nwi)] +\
-            [ '$' + str(int(gross_hourly_income(nwi, hour))) 
-            for hour in hours] for nwi in nwis]
+    table = [['(${!s}, ${!s})'.format(nwi, 52*nwi)] +\
+            ['${:.0f}'.format(gross_hourly_income(nwi, hour, weeks, tax_tiers)) 
+            for hour in hours] for nwi in net_weekly_incomes]
+
     table = [col_header] + table
     nrows = len(table)
     ncols = len(table[0])
@@ -173,12 +183,13 @@ def print_table(hours=HOURS, net_weekly_incomes=NET_WEEKLY_INCOMES):
 
     # Print the table
     print(textwrap.dedent("""\
-      +-------------------------------------------------------+
-      | Gross hourly income given                             |
-      | net (weekly, yearly) income for 52 weeks per year and |
-      | hours worked per week for %s weeks per year           |
-      +-------------------------------------------------------+""" %\
-      WORK_WEEKS_PER_YEAR))
+      ------------------------------------------------------
+      Gross hourly income given                             
+      net (weekly, yearly) income for 52 weeks per year and
+      hours worked per week for {!s} weeks per year 
+      ------------------------------------------------------""".format(
+      weeks, tax_tiers)))
+
     for i in range(nrows):
         for j in range(ncols):
             print(table[i][j].rjust(col_rjusts[j]), end='')
